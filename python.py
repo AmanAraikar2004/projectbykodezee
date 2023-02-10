af5c310415aa4b27bb4efd608162d851b467c186
AmanAraikar2004 <123152807+AmanAraikar2004@users.noreply.github.com>
2023-02-10 07:10:39 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,45 +1,62 @@
-
+import numpy as np
+import pandas as pd
 import tensorflow as tf
-from tensorflow import keras
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 # Load the dataset
-train_datagen = ImageDataGenerator(rescale = 1./255,shear_range = 0.2,zoom_range = 0.2,horizontal_flip = True)
-                                   
-                                   
-                                   
+data = np.load("waste_dataset.npy")
+labels = np.load("waste_labels.npy")
 
-training_set = train_datagen.flow_from_directory('dataset/training_set',target_size = (64, 64),batch_size = 32,class_mode = 'categorical')
-                                                 
-                                                 
-                                                 
+# Split the dataset into training and testing sets
+train_data, test_data, train_labels, test_labels = train_test_split(data, labels, test_size=0.2)
 
-test_datagen = ImageDataGenerator(rescale = 1./255)
-test_set = test_datagen.flow_from_directory('dataset/test_set',target_size = (64, 64),batch_size = 32,class_mode = 'categorical')
-                                            
-                                            
-                                            
+# Normalize the data
+train_data = train_data / 255.0
+test_data = test_data / 255.0
 
 # Build the model
-model = keras.Sequential([
-    keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(64, 64, 3)),
-    keras.layers.MaxPooling2D(2, 2),
-    keras.layers.Conv2D(64, (3,3), activation='relu'),
-    keras.layers.MaxPooling2D(2,2),
-    keras.layers.Flatten(),
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(5, activation='softmax')
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(100, 100, 3)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(16, activation='relu'),
+    tf.keras.layers.Dense(8, activation='softmax')
 ])
 
 # Compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # Train the model
-history = model.fit(training_set, epochs=25, validation_data=test_set)
+history = model.fit(train_data, train_labels, epochs=10, validation_data=(test_data, test_labels))
 
-# Evaluate the model on the test set
-test_loss, test_acc = model.evaluate(test_set)
-print('Test Accuracy: ', test_acc)
+# Evaluate the model on the test data
+test_loss, test_accuracy = model.evaluate(test_data, test_labels)
+print("Test Accuracy: {}".format(test_accuracy))
 
-# Save the model for later use
-model.save('waste_classification_model.h5')
+# Plot the accuracy and loss curves
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+plt.show()
